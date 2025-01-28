19d649a74ad519462baac0284fdecd88bd6730de
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-01-28 11:07:20 -0500

    chore(license): Switch from LGPL-3.0-only to MPL-2.0 (#25)

--- verilator/src/lib.rs
+++ verilator/src/lib.rs
@@ -1,16 +1,8 @@
 // Copyright (C) 2024 Ethan Uppal.
 //
-// This project is free software: you can redistribute it and/or modify it under
-// the terms of the GNU Lesser General Public License as published by the Free
-// Software Foundation, version 3 of the License only.
-//
-// This project is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-// details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with this project. If not, see <https://www.gnu.org/licenses/>.
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at https://mozilla.org/MPL/2.0/.
 
 use std::{
     collections::{hash_map::Entry, HashMap},
--- verilog-support/example-project/src/main.rs
+++ verilog-support/example-project/src/main.rs
@@ -1,3 +1,9 @@
+// Copyright (C) 2024 Ethan Uppal.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License,
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at https://mozilla.org/MPL/2.0/.
+
 use snafu::Whatever;
 use verilog::{verilog, VerilatorRuntime};
 
